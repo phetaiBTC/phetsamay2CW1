c67bd22523e7e68efa12ac0af1f61e607237f175
phetaiBTC <phetaibtc@gmail.com>
2024-05-20 11:31:59 +0700

    have a good day

--- App.js
+++ App.js
@@ -1,15 +1,41 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View ,Image} from 'react-native';
+import { StyleSheet, Text, View, Image, TouchableOpacity,ScrollView } from 'react-native';
 
 export default function App() {
+
+  const handlePress = () => {
+    console.log("click");
+  };
   return (
     <View style={styles.container}>
       <Image
         source={{ uri: "https://i.pinimg.com/736x/d0/18/8d/d0188d1799fc36f3ee3b9607e0f502f5.jpg" }}
         style={{ width: 200, height: 200 }}
       />
-      <Text style={{color:"red",fontSize:20}}>Mr Phetsamay Phothor 2cw1</Text>
+      <Text style={styles.text}>Mr Phetsamay Phothor 2cw1</Text>
+      <TouchableOpacity onPress={handlePress}>
+        <Text>Click here!</Text>
+      </TouchableOpacity>
+      <ScrollView contentContainerStyle={styles.containerScro}>
+        <Text style={styles.text}>Hello world</Text>
+        <Text style={styles.text}>Hello world</Text>
+        <Text style={styles.text}>Hello world</Text>
+        <Text style={styles.text}>Hello world</Text>
+        <Text style={styles.text}>Hello world</Text>
+        <Text style={styles.text}>Hello world</Text>
+        <Text style={styles.text}>Hello world</Text>
+        <Text style={styles.text}>Hello world</Text>
+        <Text style={styles.text}>Hello world</Text>
+        <Text style={styles.text}>Hello world</Text>
+        <Text style={styles.text}>Hello world</Text>
+        <Text style={styles.text}>Hello world</Text>
+        <Text style={styles.text}>Hello world</Text>
+        <Text style={styles.text}>Hello world</Text>
+        <Text style={styles.text}>Hello world</Text>
+        <Text style={styles.text}>Hello world</Text>
+      </ScrollView>
+
 
       <StatusBar style="auto" />
     </View>
@@ -23,4 +49,16 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  text: {
+    fontSize: 25,
+    color: "blue",
+    textAlign:"center"
+  },
+  containerScro:{
+    backgroundColor:"red", 
+    width:200,
+    justifyContent:"center",
+    
+
+  }
 });
